8b98dc496e187406cc4b237c6e93c7ba2e6357e8
alaazamelDev <alaa.zamel23@gmail.com>
2022-09-05 12:57:34 +0300

    initialize connection with mongodb server

--- app.js
+++ app.js
@@ -1,109 +1,38 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { mongoConnect } = require("./util/database");
 
 const app = express();
 
-const errorsController = require('./controllers/errors-controller');
-const sequelize = require('./util/database');  // pool connection
+const errorsController = require("./controllers/errors-controller");
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminData = require('./routes/admin');
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require('./routes/shop');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const Order = require('./models/order');
-const CartItem = require('./models/cart-item');
-const OrderItem = require('./models/order-item');
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // pass user object through requests
 app.use((req, res, next) => {
-    User.findByPk(1)
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => console.log(err));
+  // User.findByPk(1)
+  //     .then(user => {
+  //         req.user = user;
+  next();
+  //     })
+  //     .catch(err => console.log(err));
 });
 
-app.use('/admin', adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorsController.get404);
 
-
-/**
- * Here we will define association between 
- * items in the database.
- */
-
-/// Product(m) <=> User(1)
-User.hasMany(Product);
-Product.belongsTo(User,
-    {
-        constraints: true,
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE'
-    });
-
-/// User(1) <=> Cart(1)
-User.hasOne(Cart);
-Cart.belongsTo(User,
-    {
-        constraints: true,
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE'
-    });
-
-/// Cart(m) <=> Product(m)
-Cart.belongsToMany(Product, { through: CartItem })
-Product.belongsToMany(Cart, { through: CartItem })
-
-/// Order(m) <=> User(1)
-User.hasMany(Order);
-Order.belongsTo(User,
-    {
-        constraints: true,
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE',
-    });
-
-/// Order(m) <=> Product(m)
-Product.belongsToMany(Order, { through: OrderItem });
-Order.belongsToMany(Product, { through: OrderItem });
-
-// sync defined models with sql database
-sequelize
-    // .sync({ force: true })
-    .sync()
-    .then(() => {
-        // Create one user instance
-        User.findByPk(1)
-            .then(user => {
-                if (!user) {
-                    return User.create({
-                        name: 'Alaa Zamel',
-                        email: 'alaa.zamel80@gmail.com',
-                    }).then(user => {
-                        return user.createCart();
-                    });
-                }
-                return user;
-            })
-            .then(cart => {
-
-                app.listen(3000);
-            })
-            .catch(err => console.log(err));
-    })
-    .catch(err => {
-        console.log(err);
-    });
+mongoConnect(() => {
+  console.log("Connected");
+  app.listen(3000);
+});
--- util/database.js
+++ util/database.js
@@ -1,9 +1,31 @@
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(
-    'node-complete',
-    'root',
-    'Fbi121212@alaa.zameel',
-    { dialect: 'mysql', host: 'localhost' }
-);
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+let _database;
+
+/**
+ * used to initialize a connection to the database on fly.
+ * @param {Function(client)} callback
+ */
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://alaazamel:AOjIa7jIlHElkqFx@cluster0.bvcheuy.mongodb.net/shop?retryWrites=true"
+  )
+    .then((client) => {
+      _database = client.db("shop");
+      callback();
+    })
+    .catch((err) => console.log(err));
+};
+
+const getDatabase = () => {
+  if (_database) {
+    return _database;
+  }
+  throw "No Database is Found!";
+};
+
+module.exports = {
+  mongoConnect: mongoConnect,
+  getDatabase: getDatabase,
+};
